2e63b37f74f35e05c8d599838722e9d7b552707b
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2015-02-16 18:09:11 +0000

    Keep old-style constructors that don’t require data provider URL and description

--- web/src/main/java/uk/ac/ebi/atlas/model/baseline/BaselineExperiment.java
+++ web/src/main/java/uk/ac/ebi/atlas/model/baseline/BaselineExperiment.java
@@ -42,6 +42,15 @@ public class BaselineExperiment extends Experiment {
                        String description, String displayName,
                        Set<String> organisms, String kingdom, Map<String, String> speciesMapping,
                        boolean hasExtraInfoFile, Set<String> pubMedIds,
+                       ExperimentDesign experimentDesign, AssayGroups assayGroups) {
+        this(accession, lastUpdate, experimentalFactors, description, displayName, organisms, kingdom, speciesMapping,
+                hasExtraInfoFile, pubMedIds, experimentDesign,  assayGroups, "", "");
+    }
+
+    BaselineExperiment(String accession, Date lastUpdate, ExperimentalFactors experimentalFactors,
+                       String description, String displayName,
+                       Set<String> organisms, String kingdom, Map<String, String> speciesMapping,
+                       boolean hasExtraInfoFile, Set<String> pubMedIds,
                        ExperimentDesign experimentDesign, AssayGroups assayGroups, String dataProviderURL, String dataProviderDescription) {
         this(ExperimentType.RNASEQ_MRNA_BASELINE, accession, lastUpdate, experimentalFactors,
                 description, displayName, organisms, kingdom, speciesMapping,
@@ -53,6 +62,15 @@ public class BaselineExperiment extends Experiment {
                        String description, String displayName,
                        Set<String> organisms, String kingdom, Map<String, String> speciesMapping,
                        boolean hasExtraInfoFile, Set<String> pubMedIds,
+                       ExperimentDesign experimentDesign, AssayGroups assayGroups) {
+        this(experimentType, accession, lastUpdate, experimentalFactors, description, displayName, organisms, kingdom, speciesMapping,
+                hasExtraInfoFile, pubMedIds, experimentDesign, assayGroups, "", "");
+    }
+
+    BaselineExperiment(ExperimentType experimentType, String accession, Date lastUpdate, ExperimentalFactors experimentalFactors,
+            String description, String displayName,
+            Set < String > organisms, String kingdom, Map < String, String > speciesMapping,
+        boolean hasExtraInfoFile, Set<String> pubMedIds,
                        ExperimentDesign experimentDesign, AssayGroups assayGroups, String dataProviderURL, String dataProviderDescription) {
 
         super(experimentType, accession, lastUpdate, displayName, description,
